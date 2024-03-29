e92b6442405e91c087fd39a352b1b866e3a60ef9
mochnaufalhabbib <mochnaufalhabbib@gmail.com>
2024-03-30 00:48:53 +0700

    first commit

---
+++ latihan3.py
@@ -0,0 +1,6 @@
+#Buatkan suatu kondisi, dengan parameter jika hari sabtu maka belajar python, selain hari sabtu tidak belajar python
+Hari = "Sabtu"
+if <= "Sabtu" : 
+    print("Belajar Pyton")
+else :
+    print("Tidak Belajar Pyton")
---
+++ tugas-algoritma-phyton.py
@@ -0,0 +1,10 @@
+Berat = 5
+HargaTelur = 26000
+Ongkos = 3500 * 2
+HargaTelur = Berat * HargaTelur
+Uang = 200000
+print( Uang - ( HargaTelur + Ongkos ))
+def belanja (Berat, HargaTelur, Ongkos, uang):
+    HargaTelur = Berat * HargaTelur
+    Ongkos = Ongkos *2
+    Uang - (HargaTelur-Ongkos) 
