ea51a48c252cc24aa405c978dd1e3be56e0c572a
mochnaufalhabbib <mochnaufalhabbib@gmail.com>
2024-07-01 20:16:43 +0700

    Coffe.py

---
+++ P10_MODUL.py
@@ -0,0 +1,15 @@
+def perkalian(a,b) :
+    hasil = a * b
+    return hasil
+
+def pengurangan(a,b) :
+    hasil = a - b
+    return hasil
+
+def penjumlahan(a,b) :
+    hasil = a + b
+    return hasil
+
+def pembagian(a,b) :
+    hasil = a / b
+    return hasil
---
+++ modul_p10.py
@@ -0,0 +1,6 @@
+import P10_MODUL
+
+print(P10_MODUL.penjumlahan(10,5))
+print(P10_MODUL.pengurangan(10,5))
+print(P10_MODUL.perkalian(10,5))
+print(P10_MODUL.pembagian(10,5))
